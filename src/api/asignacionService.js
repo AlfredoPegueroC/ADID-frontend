ce725ce8ee3d4155371bf75bf9cfdc5b54dbf841
AlfredoPegueroC <alfredocapellan20@gmail.com>
2025-05-18 15:53:52 -0400

    Refactor: Simplify fetchAsignacionData by removing unused API calls and merging data handling

--- src/api/asignacionService.js
+++ src/api/asignacionService.js
@@ -6,49 +6,9 @@ export async function fetchAsignacionData(periodo = null, page = 1, searchQuery 
     const periodParam = periodo ? `&periodo=${periodo}` : "";
     const response = await fetch(`${API}api/asignacion?page=${page}${periodParam}${searchParam}`)
 
-    const [
-      asignacionResponse,
-      facultadResponse,
-      escuelaResponse,
-      docenteResponse,
-    ] = await Promise.all([
-      fetch(`${API}api/asignacion?page=${page}${periodParam}${searchParam}`),
-      fetch(`${API}facultades`),
-      fetch(`${API}escuelas`),
-      fetch(`${API}docentes`),
-    ]);
-
-    if (!asignacionResponse.ok) throw new Error("Failed to fetch asignaciones");
-    if (!facultadResponse.ok) throw new Error("Failed to fetch facultades");
-    if (!escuelaResponse.ok) throw new Error("Failed to fetch escuelas");
-    if (!docenteResponse.ok) throw new Error("Failed to fetch docentes");
-
-    const asignacionData = await asignacionResponse.json();
-    const facultadData = await facultadResponse.json();
-    const escuelaData = await escuelaResponse.json();
-    const docenteData = await docenteResponse.json();
-
-    const mergedData = asignacionData.results.map((asignacion) => {
-      const facultad = facultadData.find(
-        (fac) => fac.facultadCodigo === asignacion.facultadCodigo
-      ) || { nombre: "Facultad no encontrada" };
-
-      const escuela = escuelaData.find(
-        (esc) => esc.escuelaCodigo === asignacion.escuelaCodigo
-      ) || { nombre: "Escuela no encontrada" };
-
-      const docente = docenteData.find(
-        (doc) => doc.Docentecodigo === asignacion.DocenteCodigo
-      ) || { nombre: "Docente no encontrado" };
-
-      return {
-        ...asignacion,
-        facultadNombre: facultad.nombre,
-        escuelaNombre: escuela.nombre,
-        docenteNombre: `${docente.nombre} ${docente.apellidos}`,
-      };
-    });
-
+    if(!response.ok) throw new Error("Failed to fetch docentes");
+    const data = await response.json()
+   
     return {
       asignaciones: mergedData,
       totalPages: Math.ceil(asignacionData.count / 30),
